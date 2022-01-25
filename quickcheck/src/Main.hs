86f1a4dfc8f2757ae37dfb51c5dbe2417d3a9f7c
Paulo Gabriel Justino Bezerra <pgjbzr@gmail.com>
2022-01-25 17:44:15 -0300

    start quickcheck

---
+++ quickcheck/src/Main.hs
@@ -0,0 +1,32 @@
+module Main where
+
+import Test.QuickCheck
+import Test.QuickCheck.Property (Prop)
+import Data.List
+
+qsort :: Ord a => [a] -> [a]
+qsort [] = []
+qsort (x:xs) = qsort lhs ++ [x] ++ rhs
+  where
+      lhs = [e | e <- xs, e < x]
+      rhs = [e | e <- xs, e >= x]
+
+prop_idempotencia :: Ord a => [a] -> Bool
+prop_idempotencia xs = qsort (qsort xs) == qsort xs
+
+prop_length :: Ord a => [a] -> Bool
+prop_length xs = length (qsort xs) == length xs
+
+prop_minimum :: Ord a => [a] -> Property 
+prop_minimum xs = not (null xs)
+    ==> head (qsort xs) == minimum  xs
+
+prop_model :: Ord a => [a] -> Bool
+prop_model xs = qsort xs == sort xs
+
+-- $> quickCheck prop_idempotencia
+-- $> quickCheck prop_length
+
+main :: IO ()
+main = do
+  putStrLn "Hello World"
